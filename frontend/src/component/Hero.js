e361e510e4fd57382dfa7d4106ecf0d24bca7f4d
CEPHAL0 <xaradxarma@gmail.com>
2023-08-12 11:39:12 +0545

    Checked the Functionality of Tailwind

--- frontend/src/component/Hero.js
+++ frontend/src/component/Hero.js
@@ -1,47 +1,47 @@
-import React ,{ useState, useEffect }from 'react'
+import React, { useState, useEffect } from 'react'
 import eventpic from "../images/background.jpg"
 const Hero = () => {
-      const [show , setShow] =useState(false)
-      useEffect(() => {
-         let previousScrollPosition = 0;
-         let currentScrollPosition = 0;
-     
-         window.addEventListener('scroll', function (e) {
-     
-           // Get the new Value
-           currentScrollPosition = window.pageYOffset;
-     
-           //Subtract the two and conclude
-           if (previousScrollPosition - currentScrollPosition < 0) {
-             setShow(false);
-           } else if (previousScrollPosition - currentScrollPosition > 0) {
-             setShow(true);
-           }
-     
-           // Update the previous value
-           previousScrollPosition = currentScrollPosition;
-         });
-       }, []);
-      
+  const [show, setShow] = useState(false)
+  useEffect(() => {
+    let previousScrollPosition = 0;
+    let currentScrollPosition = 0;
+
+    window.addEventListener('scroll', function (e) {
+
+      // Get the new Value
+      currentScrollPosition = window.pageYOffset;
+
+      //Subtract the two and conclude
+      if (previousScrollPosition - currentScrollPosition < 0) {
+        setShow(false);
+      } else if (previousScrollPosition - currentScrollPosition > 0) {
+        setShow(true);
+      }
+
+      // Update the previous value
+      previousScrollPosition = currentScrollPosition;
+    });
+  }, []);
+
   return (
     <>
-    <nav className={`h-[2rem] bg-[#00A09C] p-[2rem] flex justify-center sticky top-0  z-10 items-center shadow ${show ? 'hidden' : ''}`}>
-        <ul className='text-white flex font-lato md:text-[1.5rem] text-[1rem]'> 
-            <li className='md:p-[2rem] p-[10px]'>Home</li>
-            <li className='md:p-[2rem] p-[10px]'>About us</li>
-            <li className='md:p-[2rem] p-[10px]'>Our Teams</li>
-            <li className='md:p-[2rem] p-[10px]'>Contact Us</li>
+      <nav className={`h-[2rem] bg-[#00A09C] p-[2rem] flex justify-center sticky top-0  z-10 items-center shadow ${show ? 'hidden' : ''}`}>
+        <ul className='text-white flex font-lato md:text-[1.5rem] text-[1rem]'>
+          <li className='md:p-[2rem] p-[10px]'>Home</li>
+          <li className='md:p-[2rem] p-[10px]'>About us</li>
+          <li className='md:p-[2rem] p-[10px]'>Our Teams</li>
+          <li className='md:p-[2rem] p-[10px]'>Contact Us</li>
         </ul>
-    </nav>
-    <div className='md:flex  md:m-[2rem] text-white md:w-[90vw]'>
-       <div className='md:w-[50%] h-[50vh] overflow-scroll bg-[#00A09C] rounded-[1rem] md:m-[5rem] m-[2rem]'>
+      </nav>
+      <div className='md:flex  md:m-[2rem] text-white md:w-[90vw]'>
+        <div className='md:w-[50%] h-[50vh] overflow-scroll bg-[#5ab3b1] rounded-[1rem] md:m-[5rem] m-[2rem]'>
           <h1 className='text-center  text-[3rem] m-[10px]  font-heading'>Deerwalk Data Society</h1>
           <p className='text-center font-inter text-[1.5rem] m-[10px]'>We are a group of students who are passionate about learning and exploring the latest developments in the field. We conduct workshops on a wide range of tech topics, from basic coding to cutting-edge AI, so that the students of DWIT can gain new skills and deepen their knowledge. Whether you're just starting out or are already an experienced tech expert, DWIT Software Club is the perfect place to learn, grow, and connect with others who share your interests.</p>
-       </div>
-       <div className='md:w-[50%] h-[100%] border-[1px] rounded-[2rem] md:m-[5rem] m-[2rem] overflow-hidden'>
+        </div>
+        <div className='md:w-[50%] h-[100%] border-[1px] rounded-[2rem] md:m-[5rem] m-[2rem] overflow-hidden'>
           <img src={eventpic} alt="image"></img>
-       </div>
-    </div>
+        </div>
+      </div>
     </>
   )
 }
--- frontend/src/component/Home.js
+++ frontend/src/component/Home.js
@@ -2,29 +2,31 @@ import React from 'react'
 // text-[#137598]
 import deerwalkblue from "../images/DWITblue.png"
 import datasocity from "../images/DDS.png"
+
 const Home = () => {
+
   return (
     <>
       <div className="demo-wrap">
         <div className='flex justify-center '>
           <div>
-             <img className="h-[6rem] mt-[6rem] md:pr-[2rem] pr-[1rem]" src={deerwalkblue} alt="deerwalk logo"/>
+            <img className="h-[6rem] mt-[6rem] md:pr-[2rem] pr-[1rem]" src={deerwalkblue} alt="deerwalk logo" />
           </div>
           <div className='middle-section'>
-            </div>
+          </div>
           <div>
-          <img className="h-[6rem] mt-[6rem] md:pl-[2rem] pl-[2rem]" src={datasocity} alt="deerwalk logo"/>
+            <img className="h-[6rem] mt-[6rem] md:pl-[2rem] pl-[2rem]" src={datasocity} alt="deerwalk logo" />
           </div>
         </div>
         {/* for middle text */}
         <div className='flex justify-center md:mt-[3rem] items-center' >
-            <div className=" bg-[#243c5a] m-[2rem] p-[2rem] opacity-[.88]  text-white md:text-[4rem] text-[1.5rem] rounded-lg">
+          <div className=" bg-[#243c5a] m-[2rem] p-[2rem] opacity-[.88]  text-white md:text-[4rem] text-[1.5rem] rounded-lg">
             <p className="font-lora md:pl-[8rem]">Deerwalk Data Society</p>
-            <p className='font-lato md:pl-[2rem]'>Translating And Transforming Data</p>
-            </div>
-           
+            <p className='font-lato md:pl-[2rem]' style={{ color: "red" }}>Translating And Transforming Data</p>
+          </div>
+
         </div>
-       </div>
+      </div>
     </>
   )
 }
